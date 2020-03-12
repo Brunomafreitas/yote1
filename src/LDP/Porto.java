d7b5e57f571066d8bd408aa40bd62a2d3edef157
Culverin21 <48128893+Culverin21@users.noreply.github.com>
2020-03-12 13:00:50 +0000

    Alteraçoes da aula

--- src/LDP/Porto.java
+++ src/LDP/Porto.java
@@ -18,45 +18,38 @@ import java.util.Scanner;
 public class Porto {
 
     ArrayList<Navio> navio;
-    Navio[] navios;
-    private int ct;
+    
     private int noMaxNavios;
 
-    public Porto(int noMaxNavios) {
-        ct = 0;
-        this.noMaxNavios = noMaxNavios;
-        navios = new Navio[noMaxNavios];
-        navio = new ArrayList<Navio>(noMaxNavios);
+    public Porto() {
+     navio = new ArrayList<>();
     }
 
     public int getNoMaxNavios() {
         return noMaxNavios;
     }
 
-    public Navio[] getNavios() {
-        return navios;
-    }
+    
 
     public void addNavio(Navio n) {
 
-        if (ct == noMaxNavios) {
-            System.out.println("Número máximo de navios (" + noMaxNavios + ") Atingido!!");
-        }
-        boolean confirma = false;
-        for (Navio nave : navio) {
-            if (n.equals(nave.getMatricula())) {
-                System.out.println("Navio já existe!!");
-                confirma = true;
-            }
+       if(!procura(n)){
+               System.out.println("Esse navio já existe!");
+       }
+           else{
+               
+           }
+       }
 
-            if (!confirma) {
-                navio.add(nave);
-                ct++;
-            }
+    private boolean procura(Navio n) {
+       for(Navio p : navio){
+           if(n.getMatricula().equals(p.getMatricula()))
+                return true;
+       } 
+            return false;
+       }
 
-        }
-
-    }
+    
 
     public int getCapacidadeTotalContentores() {
 
@@ -70,35 +63,43 @@ public class Porto {
         return total;
     }
 
-    public int getCapacidadeDeCarga() {
-        int total = 0;
+    public float getCapacidadeDeCarga() {
+        float total = 0;
         for (Navio p : navio) {
             if (p instanceof NavioPortaContentores) {
-                total += ((NavioPortaContentores) p).getNoMaxContentores() * 10;
+                total += ((NavioPortaContentores) p).getNoMaxContentores() * 10f;
             }
             if (p instanceof NavioPetroleiro) {
-                total += ((NavioPetroleiro) p).getCapacidadeCarga() * 10;
+                total += ((NavioPetroleiro) p).getCapacidadeCarga() * 10f;
             }
         }
         return total;
     }
+    
+    public void ListNavios(){
+        for(Navio n : navio){
+            System.out.println(n);
+        }
+    }
 
     public static void main(String[] args) {
-        Porto porto = new Porto(3);
+        Porto porto = new Porto();
 
-        NavioPetroleiro petro1 = new NavioPetroleiro("1234");
-        petro1.setCapacidadeCarga(200);
-        petro1.setComprimento(100);
-        NavioPortaContentores contenta = new NavioPortaContentores("12312");
-        contenta.setNoMaxContentores(100);
-        porto.addNavio(petro1);
-        porto.addNavio(contenta);
-        System.out.println(porto.getCapacidadeTotalContentores());
-        System.out.println("-------------------------------------------------");
-        Navio n = new Navio("666");
-        n.setComprimento(1000);
-            System.out.println("");
-            
+        Navio n = new Navio("1");
+        NavioPetroleiro nP = new NavioPetroleiro("2");
+        NavioPortaContentores nC = new NavioPortaContentores("3");
+        
+        n.setNome("n1");
+        nP.setNome("N2"); nP.setCapacidadeCarga(10);
+        nC.setNome("n3"); nC.setNoMaxContentores(10);
+        
+        porto.addNavio(n);
+        porto.addNavio(nP);
+        porto.addNavio(nC);
+        
+        porto.ListNavios();
+        
+            System.out.println("Total de carga = "+porto.getCapacidadeDeCarga());
     }
 
 }
